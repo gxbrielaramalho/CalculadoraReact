a497c23e48b4be3d646840918c0bff872c119900
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-18 20:51:05 -0300

    Desafio Criação de Calculadora com React

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button({ className, onClick, children }) {
+    return (
+        <button className={className} onClick={onClick}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
